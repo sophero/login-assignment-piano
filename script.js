b7657cefbd623df1fa7ce3757db15ab857eaf441
Sophia Di <backwardsxmarathon@gmail.com>
2018-12-08 12:31:36 -0500

    Add Piano ID login screen

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
-
+  tp = window.tp || [];
+  tp.push(['setUsePianoIdUserProvider', true]);
+  tp.push(["init", function() {
+      tp.pianoId.show({
+          disableSignUp: true,
+          displayMode: 'inline',
+          screen: 'login',
+          containerSelector: '#login-form',
+          loggedIn: function(data) {
+              console.log('user ', data.user, ' logged in with token', data.token);
+          },
+          loggedOut: function() {
+              console.log('user logged out');
+          }
+      });
+  }]);
   console.log(tp);
 });

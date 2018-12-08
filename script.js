6ff40c9212218654175692e519ab1101650405e0
Sophia Di <backwardsxmarathon@gmail.com>
2018-12-08 12:48:25 -0500

    Add logout button

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
 document.addEventListener("DOMContentLoaded", function() {
+
+  // Configure Piano ID login screen
   tp = window.tp || [];
   tp.push(['setUsePianoIdUserProvider', true]);
   tp.push(["init", function() {
-      tp.pianoId.show({
-          disableSignUp: true,
-          displayMode: 'inline',
-          screen: 'login',
-          containerSelector: '#login-form',
-          loggedIn: function(data) {
-              console.log('user ', data.user, ' logged in with token', data.token);
-          },
-          loggedOut: function() {
-              console.log('user logged out');
-          }
-      });
+    tp.pianoId.show({
+      disableSignUp: true,
+      displayMode: 'inline',
+      screen: 'login',
+      containerSelector: '#login-form',
+      loggedIn: function(data) {
+        console.log('user ', data.user, ' logged in with token', data.token);
+        logoutButton.style.display = "block";
+      },
+      loggedOut: function() {
+        console.log('user logged out');
+        logoutButton.style.display = "none";
+      }
+    });
   }]);
-  console.log(tp);
+
+  // Logout button
+  const logoutButton = document.getElementById("logout");
+  logoutButton.addEventListener("click", function() {
+    tp.pianoId.logout(() => console.log('other logout callback'));
+  });
+
+  // Check access to specific resource with RID: RVYAS7T
+
 });
